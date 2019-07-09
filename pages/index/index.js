3e4453b2818e0c1d449c04d564c4e4a06e10b3e4
joe <lovejjfg@gmail.com>
2019-07-09 14:23:00 +0800

    update hint text

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,24 +4,24 @@ const app = getApp()
 
 Page({
   data: {
-    progress_txt: '开始倒计时',
-    count: 0, // 设置 计数器 初始为0
-    totalSecond:30,
+    progress_txt: '开始',
+    count: 10, // 设置 计数器 初始为0
+    totalSecond: 10,
     countTimer: null, // 设置 定时器 初始为null
   },
-  drawProgressbg: function () {
+  drawProgressbg: function() {
     // 使用 wx.createContext 获取绘图上下文 context
     var ctx = wx.createCanvasContext('canvasProgressbg')
-    ctx.setLineWidth(8);// 设置圆环的宽度
+    ctx.setLineWidth(8); // 设置圆环的宽度
     ctx.setStrokeStyle('#20183b'); // 设置圆环的颜色
     ctx.setLineCap('round') // 设置圆环端点的形状
-    ctx.beginPath();//开始一个新的路径
+    ctx.beginPath(); //开始一个新的路径
     ctx.arc(110, 110, 100, 0, 2 * Math.PI, false);
     //设置一个原点(110,110)，半径为100的圆的路径到当前路径
-    ctx.stroke();//对当前路径进行描边
+    ctx.stroke(); //对当前路径进行描边
     ctx.draw();
   },
-  drawCircle: function (step) {
+  drawCircle: function(step) {
     var context = wx.createCanvasContext('canvasProgress');
     // 设置渐变
     var gradient = context.createLinearGradient(200, 100, 100, 200);
@@ -37,24 +37,32 @@ Page({
     context.stroke();
     context.draw()
   },
-  countInterval: function () {
+  countInterval: function() {
     this.countTimer = setInterval(() => {
-      if (this.data.count <= this.data.totalSecond) {
-        this.drawCircle(this.data.count / (this.data.totalSecond / 2))
+      this.setData({
+        progress_txt: this.data.totalSecond - this.data.count
+      });
+      if (this.data.count <= this.data.totalSecond ) {
+        this.drawCircle(this.data.count / ((this.data.totalSecond) / 2))
         this.data.count++;
       } else {
         this.setData({
-          progress_txt: "结束"
+          progress_txt: "开始"
         });
         clearInterval(this.countTimer);
       }
-      this.setData({
-        progress_txt : this.data.totalSecond - this.data.count
-      });
     }, 1000)
   },
-  onReady: function () {
+  resetClick: function(event) {
+    console.log(this.data.count);
+    if (this.data.count >= this.data.totalSecond) {
+      this.data.count = 1;
+      this.countInterval();
+    }
+
+  },
+  onReady: function() {
     this.drawProgressbg();
-    this.countInterval();
+    // this.countInterval();
   },
 })
